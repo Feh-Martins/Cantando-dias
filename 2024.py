b69a4d997dcd19da2118243534dd77cc1b4793c8
Feh-Martins <107818781+Feh-Martins@users.noreply.github.com>
2025-03-12 21:23:26 -0300

    Update 2024.py

--- 2024.py
+++ 2024.py
@@ -11,7 +11,7 @@ def get_base64_of_image(image_path):
         return base64.b64encode(img_file.read()).decode()
 
 # Caminho da imagem no computador
-image_path = "/workspaces/Cantando-dias/WhatsApp Image 2025-03-11 at 20.32.28 (1).jpeg"  # Altere para o caminho correto
+image_path = "images/WhatsApp Image 2025-03-11 at 20.32.28 (1).jpeg"  # Altere para o caminho correto
 
 # Obtém o código base64 da imagem
 base64_image = get_base64_of_image(image_path)
